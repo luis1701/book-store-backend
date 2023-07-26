098a98f731df63a505ddbafd65e29d5d37f9649e
Emmanuel <129714183+Emm4nue10@users.noreply.github.com>
2023-07-25 21:14:53 -0400

    emmanuel

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const { getAllUsers, removeUsers,  getByIdUsers, createUsers} = require('../services/bookUsers');
+var router = express.Router();
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+  const {  } = req.query
+  const resultUsers = await getAllUsers()
+  res.send(resultUsers);
 });
+
+router.get("/:id", async function(req, res) {
+  const { params } = req
+  const { id } = params
+  const result = await getByIdUsers(id)
+  res.send(result)
+})
+
+
+router.delete("/:id", async function(req, res) {
+  const { params } = req
+  const { id } = params
+  const resultUsers = await removeUsers(id)
+  res.send(resultUsers)
+})
+
+router.post('/', async function(req, res) {
+  const { body } = req
+  const result = await createUsers(body)
+  res.send(result);
+});
+
 
 module.exports = router;
